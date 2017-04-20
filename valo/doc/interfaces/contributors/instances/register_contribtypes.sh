bd26f8f37bff9c66a603fb9eb1971f76ccff5b2f
Alvaro Santamaria <asantamaria@itrsgroup.com>
2017-04-20 10:01:21 +0200

    -

--- valo/doc/interfaces/contributors/instances/register_contribtypes.sh
+++ valo/doc/interfaces/contributors/instances/register_contribtypes.sh
@@ -3,8 +3,6 @@
 ###########################################
 curl http://localhost:8888/contributors/demo/iot_board/instances/board-00001 -X PUT --data @instance_iot_board_01.json -H "Content-Type: application/json"
 
-
-
 ###########################################
 # Update contributor type
 ###########################################
