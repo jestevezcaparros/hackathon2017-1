c2188ca33d74bf557a01352235a570dbdf9d2a6a
Alvaro Santamaria <asantamaria@itrsgroup.com>
2017-05-02 12:04:17 +0200

    transport example added

---
+++ valo/src/transport_to_valo_bridge_js/examples/mqtt_test.js
@@ -0,0 +1,27 @@
+import mqtt from 'mqtt';
+
+const client = mqtt.connect('mqtt://iot.eclipse.org');
+const QUEUES = [
+    '/jonthebeach/sensors/temperature',
+    '/jonthebeach/sensors/humidity',
+    '/jonthebeach/sensors/alcohol',
+    '/jonthebeach/sensors/light',
+    '/jonthebeach/sensors/distance'
+];
+
+
+client.on('connect', () => {
+    console.log('connected');
+    client.subscribe('presence');
+    client.subscribe('/jonthebeach/sensors/temperature');
+    // Subscribe to all the sensors' queues
+    QUEUES.forEach( q => client.subscribe(q));
+    setTimeout( () => client.publish('presence', "Hello"), 2000);
+});
+client.on('message', (topic, message) => {
+    // Message is buffer
+    console.log(topic, message.toString());
+});
+client.on('error', function() {
+    console.error('ERROR');
+});
