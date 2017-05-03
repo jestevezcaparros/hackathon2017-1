f16d8f758af3f9b6c355e1dd19e81f7179923417
Alvaro Santamaria <asantamaria@itrsgroup.com>
2017-05-03 11:02:15 +0200

    query fix

--- valo/doc/interfaces/queries/query_mobile_happiness_avg.sql
+++ valo/doc/interfaces/queries/query_mobile_happiness_avg.sql
@@ -1,4 +1,4 @@
 from /streams/demo/mobile/happiness
 group by contributor.user.typeOfParticipant
-select typeOfParticipant as TypeOfParticipant, avg( (toDouble(happiness)+1.0) / 2.0) as AverageHappiness
+select typeOfParticipant as TypeOfParticipant, avg( (happiness+1.0) / 2.0) as AverageHappiness
 -- AverageHappiness shouyd be in interval [0.0, 1.0]
